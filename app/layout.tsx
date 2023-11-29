e169db559110835067f680c1dc5d4555049c5080
Peter Karolyi <peter@karolyi.dev>
2023-11-29 13:31:50 +0100

    add authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import { auth } from "@/lib/auth";
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
+import { redirect } from "next/navigation";
 import "./globals.css";
 
 const lato = Lato({ weight: ["400", "700"], subsets: ["latin"] });
@@ -8,14 +10,18 @@ export const metadata: Metadata = {
   title: "CPPFTW Archive | BETA",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={lato.className}>{children}</body>
-    </html>
-  );
+  const session = await auth();
+
+  if (!session) redirect("/api/auth/signin");
+  else
+    return (
+      <html lang="en">
+        <body className={lato.className}>{children}</body>
+      </html>
+    );
 }
---
+++ lib/auth.ts
@@ -0,0 +1,33 @@
+import type {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
+import type { NextAuthOptions } from "next-auth";
+import { getServerSession } from "next-auth";
+import SlackProvider from "next-auth/providers/slack";
+
+export const authConfig = {
+  providers: [
+    SlackProvider({
+      clientId: process.env.SLACK_CLIENT_ID!,
+      clientSecret: process.env.SLACK_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    signIn(params: any) {
+      console.log(params);
+      const teamId: any = params?.profile?.["https://slack.com/team_id"];
+      return teamId === process.env.SLACK_TEAM_ID!;
+    },
+  },
+} satisfies NextAuthOptions;
+
+export function auth(
+  ...args:
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) {
+  return getServerSession(...args, authConfig);
+}
