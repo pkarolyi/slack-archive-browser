82cc1c12fe1916f9d7b810a51f0a668cc04b8442
Peter Karolyi <peter@karolyi.dev>
2025-01-26 15:26:05 +0100

    separate thread replies from main message

--- components/ui/message.tsx
+++ components/ui/message.tsx
@@ -29,46 +29,50 @@ export default function Message({
   }, [highlighted, messageRef]);
 
   return (
-    <div
-      className={clsx(
-        "group/message relative rounded-md px-2 py-1 text-stone-800",
-        highlighted
-          ? "my-1 border border-cyan-600 bg-cyan-50 hover:bg-cyan-100"
-          : "hover:bg-stone-200",
-      )}
-      ref={messageRef}
-    >
-      <MeassageActions message={message} />
-      <div className="flex flex-row items-start gap-2">
-        {message.user.imageUrl && (
-          <div className="mt-1 flex-none">
-            <Image
-              alt={`Profile picture of ${message.user.name}`}
-              src={message.user.imageUrl}
-              className="h-[36px] w-[36px] rounded-md lg:h-[42px] lg:w-[42px]"
-              width={42}
-              height={42}
-            />
-          </div>
+    <>
+      <div
+        className={clsx(
+          "group/message relative rounded-md px-2 py-1 text-stone-800",
+          highlighted
+            ? "my-1 border border-cyan-600 bg-cyan-50 hover:bg-cyan-100"
+            : "hover:bg-stone-200",
         )}
-        <div className="flex flex-1 flex-col">
-          <div className="flex gap-2 text-sm lg:text-base">
-            <span className="font-bold">{message.user.name}</span>
-            <Timestamp date={message.isoDate} />
+        ref={messageRef}
+      >
+        <MeassageActions message={message} />
+        <div className="flex flex-row items-start gap-2">
+          {message.user.imageUrl && (
+            <div className="mt-1 flex-none">
+              <Image
+                alt={`Profile picture of ${message.user.name}`}
+                src={message.user.imageUrl}
+                className="h-[36px] w-[36px] rounded-md lg:h-[42px] lg:w-[42px]"
+                width={42}
+                height={42}
+              />
+            </div>
+          )}
+          <div className="flex flex-1 flex-col">
+            <div className="flex gap-2 text-sm lg:text-base">
+              <span className="font-bold">{message.user.name}</span>
+              <Timestamp date={message.isoDate} />
+            </div>
+            <p className="text-sm whitespace-pre-line lg:text-base">
+              {message.text}
+            </p>
+            <MessageReactions reactions={message.reactions} />
           </div>
-          <p className="text-sm whitespace-pre-line lg:text-base">
-            {message.text}
-          </p>
-          <MessageReactions reactions={message.reactions} />
-          {message.threadReplies?.map((reply) => (
-            <Message
-              key={reply.id}
-              message={{ ...reply, threadReplies: [] }}
-              highlightedTs={highlightedTs}
-            />
-          ))}
         </div>
       </div>
-    </div>
+      <div className="ml-16">
+        {message.threadReplies?.map((reply) => (
+          <Message
+            key={reply.id}
+            message={{ ...reply, threadReplies: [] }}
+            highlightedTs={highlightedTs}
+          />
+        ))}
+      </div>
+    </>
   );
 }
